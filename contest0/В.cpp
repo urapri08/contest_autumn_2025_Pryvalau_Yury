b2784d23e5473853da3fcfb0e9cbcf4e74b15207
urapri08 <urapri08@gmail.com>
2025-10-03 00:23:36 +0300

    Update В.cpp

--- contest0/В.cpp
+++ contest0/В.cpp
@@ -3,41 +3,28 @@
 #include <iomanip>
 #include <iostream>
 #include <vector>
+const int32_t cTen = 10;
 int main() {
-  float right;
-  float left;
+  int32_t right;
+  int32_t left;
   int32_t size_input;
   int32_t size_prom;
   std::cin >> size_input;
-  std::cout.precision(2 * 5);
+  std::cout.precision(cTen);
   std::cout << std::fixed;
-  std::vector<long double> input(size_input);
-  std::vector<long double> pref(size_input);
-  for (int32_t i = 0; i < size_input; i++) {
+  std::vector<long double> input(size_input + 1);
+  std::vector<long double> pref(size_input + 1);
+  for (int32_t i = 1; i <= size_input; i++) {
     std::cin >> input[i];
   }
-  pref[0] = log2(input[0]);
-  for (int32_t i = 1; i < size_input; i++) {
+  pref[0] = 0;
+  for (int32_t i = 1; i <= size_input; i++) {
     pref[i] = pref[i - 1] + log2(input[i]);
   }
   std::cin >> size_prom;
-  std::vector<int32_t> prom(size_prom * 2);
-  for (int32_t z = 0; z < size_prom * 2; z++) {
-    std::cin >> prom[z];
-  }
-  for (int32_t b = 1; b < size_prom * 2; b += 2) {
-    left = prom[b - 1];
-    right = prom[b];
-    if (left != right && left != 0) {
-      std::cout << pow(2, (pref[right] - pref[left - 1]) / (right - left + 1.0))
-        << "\n";
-    }
-    else if (left == right) {
-      std::cout << input[right] << "\n";
-    }
-    else {
-      std::cout << pow(2, (pref[right]) / (right + 1.0)) << "\n";
-    }
+  for (int32_t b = 0; b < size_prom; b++) {
+    std::cin >> left >> right;
+    std::cout << pow(2, (pref[right + 1] - pref[left]) / (right - left + 1.0))
+      << "\n";
   }
   return 0;
-}
