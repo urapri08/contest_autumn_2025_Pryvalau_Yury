e9cad83f4a8a57265aabd1d7b8538179d1684844
urapri08 <urapri08@gmail.com>
2026-03-21 17:46:51 +0300

    Update K.cpp

--- contest5/K.cpp
+++ contest5/K.cpp
@@ -1,34 +1,29 @@
 ﻿#include <iostream>
+#include <vector>
 const int cZero = 0;
 const int cOne = 1;
 const int cTwo = 2;
-const int cMaxN = 250005;
-int global_n;
-int a[cMaxN];
-int d[cMaxN];
-int p[cMaxN];
-int ans[cMaxN];
-void ReadInput() {
-  std::cin >> global_n;
-  for (int i = cOne; i <= global_n; i++) {
+const int cMaxN = 250'005;
+
+void ReadInput(int& n, std::vector<int>& a) {
+  std::cin >> n;
+  a.resize(n + cOne);
+  for (int i = cOne; i <= n; i++) {
     std::cin >> a[i];
   }
 }
-void SolveProblem() {
-  int max_len;
-  max_len = cZero;
-  for (int i = cOne; i <= global_n; i++) {
-    int left;
-    left = cOne;
-    int right;
-    right = max_len;
-    int pos;
-    pos = max_len + cOne;
-    int x;
-    x = a[i];
+
+void SolveProblem(int n, const std::vector<int>& a) {
+  int max_len = cZero;
+  std::vector<int> d(n + cOne, cZero);
+  std::vector<int> p(n + cOne, cZero);
+  for (int i = cOne; i <= n; i++) {
+    int left = cOne;
+    int right = max_len;
+    int pos = max_len + cOne;
+    int x = a[i];
     while (left <= right) {
-      int mid;
-      mid = left + (right - left) / cTwo;
+      int mid = left + (right - left) / cTwo;
       if (a[d[mid]] < x) {
         pos = mid;
         right = mid - cOne;
@@ -43,11 +38,10 @@ void SolveProblem() {
       max_len = pos;
     }
   }
-  std::cout << max_len << "\n";
-  int curr;
-  curr = d[max_len];
-  int k;
-  k = cZero;
+  std::cout << max_len << '\n';
+  int curr = d[max_len];
+  int k = cZero;
+  std::vector<int> ans(n + cOne, cZero);
   while (curr != cZero) {
     ans[k] = curr;
     k++;
@@ -59,12 +53,16 @@ void SolveProblem() {
       std::cout << " ";
     }
   }
-  std::cout << "\n";
+  std::cout << '\n';
 }
+
 void Solve() {
-  ReadInput();
-  SolveProblem();
+  int n = cZero;
+  std::vector<int> a;
+  ReadInput(n, a);
+  SolveProblem(n, a);
 }
+
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
