587a21fbff5692db728b5b81c322f1655d27d3f7
urapri08 <urapri08@gmail.com>
2026-05-06 20:56:52 +0300

    Update D.cpp

--- contest6/D.cpp
+++ contest6/D.cpp
@@ -1,84 +1,72 @@
 #include <iostream>
 #include <vector>
-const int cZero = 0;
-const int cOne = 1;
+
 void DfsFirstPass(int current_node,
   const std::vector<std::vector<int>>& adjacency_list,
   std::vector<int>& visited_nodes,
   std::vector<int>& exit_order) {
-  visited_nodes[current_node] = cOne;
-  int neighbor_count = adjacency_list[current_node].size();
-  int neighbor_index = cZero;
-  while (neighbor_index < neighbor_count) {
-    int next_node = adjacency_list[current_node][neighbor_index];
-    if (visited_nodes[next_node] == cZero) {
+  visited_nodes[current_node] = 1;
+  for (int next_node : adjacency_list[current_node]) {
+    if (visited_nodes[next_node] == 0) {
       DfsFirstPass(next_node, adjacency_list, visited_nodes, exit_order);
     }
-    neighbor_index++;
   }
   exit_order.push_back(current_node);
 }
+
 void DfsSecondPass(int current_node,
   const std::vector<std::vector<int>>& reversed_graph,
   std::vector<int>& component_ids, int current_id) {
   component_ids[current_node] = current_id;
-  int neighbor_count = reversed_graph[current_node].size();
-  int neighbor_index = cZero;
-  while (neighbor_index < neighbor_count) {
-    int next_node = reversed_graph[current_node][neighbor_index];
-    if (component_ids[next_node] == cZero) {
+  for (int next_node : reversed_graph[current_node]) {
+    if (component_ids[next_node] == 0) {
       DfsSecondPass(next_node, reversed_graph, component_ids, current_id);
     }
-    neighbor_index++;
   }
 }
+
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
-  int vertex_count = cZero;
-  int edge_count = cZero;
-  std::cin >> vertex_count;
-  std::cin >> edge_count;
-  std::vector<std::vector<int>> adjacency_list(vertex_count + cOne);
-  std::vector<std::vector<int>> reversed_graph(vertex_count + cOne);
-  int edge_index = cZero;
-  while (edge_index < edge_count) {
-    int from_vertex = cZero;
-    int to_vertex = cZero;
-    std::cin >> from_vertex;
-    std::cin >> to_vertex;
+
+  int vertex_count = 0;
+  int edge_count = 0;
+  std::cin >> vertex_count >> edge_count;
+
+  std::vector<std::vector<int>> adjacency_list(vertex_count + 1);
+  std::vector<std::vector<int>> reversed_graph(vertex_count + 1);
+
+  for (int i = 0; i < edge_count; ++i) {
+    int from_vertex = 0;
+    int to_vertex = 0;
+    std::cin >> from_vertex >> to_vertex;
     adjacency_list[from_vertex].push_back(to_vertex);
     reversed_graph[to_vertex].push_back(from_vertex);
-    edge_index++;
   }
-  std::vector<int> visited_nodes(vertex_count + cOne, cZero);
+
+  std::vector<int> visited_nodes(vertex_count + 1, 0);
   std::vector<int> exit_order;
-  int start_node = cOne;
-  while (start_node <= vertex_count) {
-    if (visited_nodes[start_node] == cZero) {
-      DfsFirstPass(start_node, adjacency_list, visited_nodes, exit_order);
+  for (int i = 1; i <= vertex_count; ++i) {
+    if (visited_nodes[i] == 0) {
+      DfsFirstPass(i, adjacency_list, visited_nodes, exit_order);
     }
-    start_node++;
   }
-  std::vector<int> component_ids(vertex_count + cOne, cZero);
-  int current_id = cZero;
-  int order_index = exit_order.size();
-  order_index--;
-  while (order_index >= cZero) {
-    int check_node = exit_order[order_index];
-    if (component_ids[check_node] == cZero) {
+
+  std::vector<int> component_ids(vertex_count + 1, 0);
+  int current_id = 0;
+  for (int i = vertex_count - 1; i >= 0; --i) {
+    int check_node = exit_order[i];
+    if (component_ids[check_node] == 0) {
       current_id++;
       DfsSecondPass(check_node, reversed_graph, component_ids, current_id);
     }
-    order_index--;
   }
+
   std::cout << current_id << "\n";
-  int print_node = cOne;
-  while (print_node <= vertex_count) {
-    std::cout << component_ids[print_node];
-    std::cout << " ";
-    print_node++;
+  for (int i = 1; i <= vertex_count; ++i) {
+    std::cout << component_ids[i] << (i == vertex_count ? "" : " ");
   }
   std::cout << "\n";
-  return cZero;
+
+  return 0;
 }
