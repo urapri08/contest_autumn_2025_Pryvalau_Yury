0fdd4ab8e3fb615a02f59d24018c6e0cc8ca22a4
urapri08 <urapri08@gmail.com>
2026-05-06 20:58:58 +0300

    Update E.cpp

--- contest6/E.cpp
+++ contest6/E.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <vector>
+
 struct Edge {
   int to;
   int id;
 };
+
 void QuickSort(std::vector<int>& arr, int left, int right) {
   if (left >= right) {
     return;
   }
-  int pivot_index = right - left;
-  pivot_index /= 2;
-  pivot_index += left;
+  int pivot_index = left + (right - left) / 2;
   int pivot = arr[pivot_index];
   int i = left;
   int j = right;
@@ -22,9 +22,7 @@ void QuickSort(std::vector<int>& arr, int left, int right) {
       j--;
     }
     if (i <= j) {
-      int temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+      std::swap(arr[i], arr[j]);
       i++;
       j--;
     }
@@ -32,20 +30,21 @@ void QuickSort(std::vector<int>& arr, int left, int right) {
   QuickSort(arr, left, j);
   QuickSort(arr, i, right);
 }
-void FindBridges(int current_node, int parent_edge,
+
+void FindBridges(
+  int current_node,
+  int parent_edge,
   const std::vector<std::vector<Edge>>& adj,
-  std::vector<int>& time_in, std::vector<int>& low_time,
+  std::vector<int>& time_in,
+  std::vector<int>& low_time,
   std::vector<int>& bridges) {
   time_in[current_node] = time_in[0];
   low_time[current_node] = time_in[0];
   time_in[0]++;
-  int neighbor_count = adj[current_node].size();
-  int neighbor_index = 0;
-  while (neighbor_index < neighbor_count) {
-    int next_node = adj[current_node][neighbor_index].to;
-    int edge_id = adj[current_node][neighbor_index].id;
+  for (const auto& edge : adj[current_node]) {
+    int next_node = edge.to;
+    int edge_id = edge.id;
     if (edge_id == parent_edge) {
-      neighbor_index++;
       continue;
     }
     if (time_in[next_node] != 0) {
@@ -62,56 +61,46 @@ void FindBridges(int current_node, int parent_edge,
         bridges.push_back(edge_id);
       }
     }
-    neighbor_index++;
   }
 }
+
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
+
   int vertex_count = 0;
   int edge_count = 0;
-  std::cin >> vertex_count;
-  std::cin >> edge_count;
+  std::cin >> vertex_count >> edge_count;
+
   std::vector<std::vector<Edge>> adj(vertex_count + 1);
-  int edge_index = 1;
-  while (edge_index <= edge_count) {
+  for (int edge_index = 1; edge_index <= edge_count; ++edge_index) {
     int from_vertex = 0;
     int to_vertex = 0;
-    std::cin >> from_vertex;
-    std::cin >> to_vertex;
-    Edge forward_edge;
-    forward_edge.to = to_vertex;
-    forward_edge.id = edge_index;
-    adj[from_vertex].push_back(forward_edge);
-    Edge backward_edge;
-    backward_edge.to = from_vertex;
-    backward_edge.id = edge_index;
-    adj[to_vertex].push_back(backward_edge);
-    edge_index++;
+    std::cin >> from_vertex >> to_vertex;
+    adj[from_vertex].push_back(Edge{ to_vertex, edge_index });
+    adj[to_vertex].push_back(Edge{ from_vertex, edge_index });
   }
+
   std::vector<int> time_in(vertex_count + 1, 0);
   std::vector<int> low_time(vertex_count + 1, 0);
   std::vector<int> bridges;
   time_in[0] = 1;
-  int start_node = 1;
-  while (start_node <= vertex_count) {
+  for (int start_node = 1; start_node <= vertex_count; ++start_node) {
     if (time_in[start_node] == 0) {
       FindBridges(start_node, -1, adj, time_in, low_time, bridges);
     }
-    start_node++;
   }
-  int bridges_count = bridges.size();
+
+  int bridges_count = static_cast<int>(bridges.size());
   std::cout << bridges_count << "\n";
+
   if (bridges_count > 0) {
-    int max_index = bridges_count - 1;
-    QuickSort(bridges, 0, max_index);
-    int print_index = 0;
-    while (print_index < bridges_count) {
-      std::cout << bridges[print_index];
-      std::cout << " ";
-      print_index++;
+    QuickSort(bridges, 0, bridges_count - 1);
+    for (int i = 0; i < bridges_count; ++i) {
+      std::cout << bridges[i] << (i == bridges_count - 1 ? "" : " ");
     }
     std::cout << "\n";
   }
+
   return 0;
 }
